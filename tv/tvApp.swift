a71e0b438c3fb5efcb143675039a4e5e42cf22e2
iqbal <ifachry@rocketmail.com>
2023-08-03 15:49:00 +0700

    Initial Commit

---
+++ tv/tvApp.swift
@@ -0,0 +1,17 @@
+//
+//  tvApp.swift
+//  tv
+//
+//  Created by Iqbal Fachry on 03/08/23.
+//
+
+import SwiftUI
+
+@main
+struct tvApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
